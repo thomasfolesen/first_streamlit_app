3e00fec9f0e9eaeba878c9efec703b5cf11536b1
thomasfolesen <143394389+thomasfolesen@users.noreply.github.com>
2023-08-28 11:43:16 +0200

    Update streamlit_app.py
    typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list.loc[fruits_selected])
