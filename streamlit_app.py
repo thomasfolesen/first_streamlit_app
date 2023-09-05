f3f5969f980f31106bfd10c799b32be3aabe8fbd
thomasfolesen <143394389+thomasfolesen@users.noreply.github.com>
2023-09-05 14:12:14 +0200

    Update streamlit_app.py
    Inserted to snowflake account

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display the JSON in normalized table visual form
 streamlit.dataframe(fruityvice_normalized)
 
+#snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
