b029a3d851f329d766c52abe677a772c209a8fbf
thomasfolesen <143394389+thomasfolesen@users.noreply.github.com>
2023-09-06 12:33:47 +0200

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,18 +33,16 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 streamlit.header('Fruity Fruit Advice!')
-streamlit.dataframe(get_fruityvice_data("Kiwi"))
-"""
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.text("if not")
     streamlit.error("Please select a fruit to get information")
   else:
     streamlit.text("else")
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-"""
+
 #don't run anything under here while we troubleshoot
 streamlit.stop()
 
