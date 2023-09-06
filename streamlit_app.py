390a41b2b6fa0482ed94e76e997b33e1fb58d585
thomasfolesen <143394389+thomasfolesen@users.noreply.github.com>
2023-09-06 12:43:00 +0200

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
     streamlit.dataframe(back_from_function)
 
 # snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
